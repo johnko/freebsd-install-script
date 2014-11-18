b4469fb653d907444973106c26380725d670ee39
John Ko <git@johnko.ca>
2014-11-18 03:40:42 -0500

    initial commit

---
+++ freebsd-zfs-geli-pwless.sh
@@ -0,0 +1,344 @@
+#!/bin/sh
+
+######################################################################
+# Script version is YYmmdd-HHMM in UTC, date +%y%m%d-%H%M%S
+######################################################################
+SCRIPTVERSION=141118-034013
+
+######################################################################
+# Variables you can edit / pass
+######################################################################
+
+: ${rpool:=rpool}
+: ${bpool:=bpool}
+: ${disks:="ada0"}
+: ${type:=stripe}
+: ${mnt:=/mnt}
+: ${bsize:=1g}
+: ${ssize:=2g}
+: ${release:=10.1-RELEASE}
+
+######################################################################
+# Usage
+######################################################################
+
+usage() {
+  cat <<EOF
+usage: $0 [-f] [mfsroot]
+       -f       Force export of existing pool.
+       -v       Version.
+       mfsroot  Create mfsroot type of system.
+
+examples:
+
+  Install on mirror disks:
+       disks="ada0 ada1" type=mirror $0
+
+  Make a bootable ZFS USB, which loads as mfs:
+       disks=da0 rpool=rusb bpool=busb $0 mfsroot
+  Note we change the pool name so they don't conflict.
+EOF
+  exit
+}
+
+######################################################################
+# Options
+######################################################################
+
+while [ "$1" ]; do
+  case "$1" in
+    "mfsroot")  MAKEMFSROOT=1 ; shift ;;
+    "-f")       FORCEEXPORT=1 ; shift ;;
+    "-v")       echo $SCRIPTVERSION ; exit ;;
+    *)          usage ;;
+  esac
+done
+
+######################################################################
+# If force, delete pools and detach partition 3 and 4
+######################################################################
+
+if [ "$FORCEEXPORT" ]; then
+  zpool export -f $bpool # have to export bpool before rpool
+  zpool export -f $rpool
+  for D in $disks ; do geli detach ${D}p3 ; geli detach ${D}p4 ; done
+fi
+
+######################################################################
+# Quit if pools exist
+######################################################################
+
+if zfs list $rpool >/dev/null ; then
+  echo "ERROR: A pool named $rpool already exists."
+  exit 1
+fi
+if zfs list $bpool >/dev/null ; then
+  echo "ERROR: A pool named $bpool already exists."
+  exit 1
+fi
+
+######################################################################
+# How to create a b64 patch
+######################################################################
+# diff -u zfsboot /usr/libexec/bsdinstall/zfsboot | b64encode -
+
+######################################################################
+# Patch zfsboot for passwordless (-P,-p) geli
+######################################################################
+
+chmod 755 /usr/libexec/bsdinstall/zfsboot
+
+b64decode -o /dev/stdout <<EOF | patch -N -l /usr/libexec/bsdinstall/zfsboot
+begin-base64 644 -
+LS0tIHpmc2Jvb3QJMjAxNC0xMS0xNyAwNDo0Mzo1NS44MTg5NjE4ODIgKzAwMDAKKysrIC91c3Iv
+bGliZXhlYy9ic2RpbnN0YWxsL3pmc2Jvb3QJMjAxNC0xMS0xNyAwNDozNTo0NS40MTY5OTU2MjQg
+KzAwMDAKQEAgLTE4Miw5ICsxODIsOSBAQAogQ0hNT0RfTU9ERT0nY2htb2QgJXMgIiVzIicKIERE
+X1dJVEhfT1BUSU9OUz0nZGQgaWY9IiVzIiBvZj0iJXMiICVzJwogRUNIT19BUFBFTkQ9J2VjaG8g
+IiVzIiA+PiAiJXMiJwotR0VMSV9BVFRBQ0g9J2dlbGkgYXR0YWNoIC1qIC0gLWsgIiVzIiAiJXMi
+JworR0VMSV9BVFRBQ0g9J2dlbGkgYXR0YWNoIC1wIC1rICIlcyIgIiVzIicKIEdFTElfREVUQUNI
+X0Y9J2dlbGkgZGV0YWNoIC1mICIlcyInCi1HRUxJX1BBU1NXT1JEX0lOSVQ9J2dlbGkgaW5pdCAt
+YiAtQiAiJXMiIC1lICVzIC1KIC0gLUsgIiVzIiAtbCAyNTYgLXMgNDA5NiAiJXMiJworR0VMSV9Q
+QVNTV09SRF9JTklUPSdnZWxpIGluaXQgLWIgLUIgIiVzIiAtZSAlcyAtUCAtSyAiJXMiIC1sIDI1
+NiAtcyA0MDk2ICIlcyInCiBHTk9QX0NSRUFURT0nZ25vcCBjcmVhdGUgLVMgNDA5NiAiJXMiJwog
+R05PUF9ERVNUUk9ZPSdnbm9wIGRlc3Ryb3kgIiVzIicKIEdQQVJUX0FERD0nZ3BhcnQgYWRkIC10
+ICVzICIlcyInCkBAIC0xMTUwLDE1ICsxMTUwLDYgQEAKIAkjIENyZWF0ZSB0aGUgZ2VsaSg4KSBH
+RU9NUwogCSMKIAlpZiBbICIkWkZTQk9PVF9HRUxJX0VOQ1JZUFRJT04iIF07IHRoZW4KLQkJIyBQ
+cm9tcHQgdXNlciBmb3IgcGFzc3dvcmQgKHR3aWNlKQotCQlpZiAhIG1zZ19lbnRlcl9uZXdfcGFz
+c3dvcmQ9IiRtc2dfZ2VsaV9wYXNzd29yZCIgXAotCQkJZl9kaWFsb2dfaW5wdXRfcGFzc3dvcmQK
+LQkJdGhlbgotCQkJZl9kcHJpbnRmICIkZnVuY25hbWU6IFVzZXIgY2FuY2VsbGVkIgotCQkJZl9z
+aG93X2VyciAiJG1zZ191c2VyX2NhbmNlbGxlZCIKLQkJCXJldHVybiAkRkFJTFVSRQotCQlmaQot
+CiAJCSMgSW5pdGlhbGl6ZSBnZWxpKDgpIG9uIGVhY2ggb2YgdGhlIHRhcmdldCBwYXJ0aXRpb25z
+CiAJCWZvciBkaXNrIGluICRkaXNrczsgZG8KIAkJCWZfZGlhbG9nX2luZm8gIiRtc2dfZ2VsaV9z
+ZXR1cCIgXAo=
+====
+EOF
+
+chmod 555 /usr/libexec/bsdinstall/zfsboot
+
+######################################################################
+# load geli and remove past bsdinstall temporary files
+######################################################################
+
+geli load
+
+rm -r /tmp/bsdinstall*
+
+######################################################################
+# Perform creation of zfsboot
+######################################################################
+
+ZFSBOOT_DISKS="$disks" \
+ZFSBOOT_VDEV_TYPE=$type \
+ZFSBOOT_POOL_NAME=$rpool \
+ZFSBOOT_BEROOT_NAME=rfs \
+ZFSBOOT_GELI_ENCRYPTION=1 \
+ZFSBOOT_BOOT_POOL_NAME=$bpool \
+ZFSBOOT_BOOT_POOL_SIZE=$bsize \
+ZFSBOOT_SWAP_SIZE=$ssize \
+ZFSBOOT_SWAP_ENCRYPTION=1 \
+nonInteractive=0 \
+bsdinstall zfsboot
+
+######################################################################
+# Copy the generated /boot/loader.conf and /etc/fstab
+######################################################################
+
+cat /tmp/bsdinstall_boot/loader.conf.* > /mnt/boot/loader.conf
+chmod 644 /mnt/boot/loader.conf
+install -d -m 755 /mnt/etc
+install    -m 644 /tmp/bsdinstall_etc/fstab /mnt/etc/fstab
+
+######################################################################
+# Check if local distribution exists, if so copy to mnt
+######################################################################
+
+########## this notation /boot/.. is in case /boot is a symlink (eg. mfsroot)
+distdir=/boot/../${release}
+if [ ! -e $distdir/kernel.txz -o ! -e $distdir/base.txz ]; then
+distdir=/mnt/boot/../${release}
+else
+install -d -m 755 /mnt/${release}
+tar cf - -C /boot/.. ${release} | tar -C /mnt/boot/.. -xf -
+fi
+install -d -m 755 $distdir
+
+######################################################################
+# Fetch distribution if no local copy exists
+######################################################################
+
+if [ ! -e $distdir/kernel.txz -o ! -e $distdir/base.txz ]; then
+DISTRIBUTIONS="kernel.txz base.txz" \
+BSDINSTALL_DISTDIR=$distdir \
+BSDINSTALL_DISTSITE="http://ftp4.freebsd.org/pub/FreeBSD/releases/`uname -m`/`uname -p`/${release}" \
+nonInteractive=0 \
+bsdinstall distfetch
+fi
+
+######################################################################
+# Extract ditribution
+######################################################################
+
+DISTRIBUTIONS="kernel.txz base.txz" \
+BSDINSTALL_DISTDIR=$distdir \
+BSDINSTALL_CHROOT=$mnt \
+nonInteractive=0 \
+bsdinstall distextract
+
+######################################################################
+# Set some loader.conf options
+######################################################################
+
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" aesni_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" ahci_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" autoboot_delay="1"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" geom_eli_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" geom_label_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" geom_mirror_load="YES"
+########## hw.bge.allow_asf for my HP server to stop network disconnect
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "hw.bge.allow_asf=0"
+########## hw.usb.no_shutdown_wait allows USB not to stall poweroff
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "hw.usb.no_shutdown_wait=1"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" if_lagg_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "kern.cam.boot_delay=10000"
+########## I prefer no disk ID
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "kern.geom.label.disk_ident.enable=0"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "kern.maxfiles=65530"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "kern.maxswzone=512M"
+########## loader_logo should stop logo from showing, appears to be BROKEN
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" loader_logo="none"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" nullfs_load="YES"
+########## tmpfs_load for mfsroot /usr
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" tmpfs_load="YES"
+########## vfs.zfs.arc_max limit arc usage on low RAM systems
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "vfs.zfs.arc_max=256M"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" zfs_load="YES"
+
+######################################################################
+# Set some rc.conf options
+######################################################################
+
+#	/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" ntpdate_hosts=""
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" auditd_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" ftpproxy_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" ntpdate_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" openntpd_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" pf_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" pf_rules="/server/pf/pf.conf"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" pflog_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" pflog_logfile="/var/log/pflog"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sendmail_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sendmail_submit_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sendmail_outbound_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sendmail_msp_queue_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sshd_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sshd_rsa1_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" sshd_dsa_enable="NO"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" syslogd_flags="-s -b127.0.0.1"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" zfs_enable="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" virtio_load="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" virtio_pci_load="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" virtio_blk_load="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" if_vtnet_load="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" virtio_balloon_load="YES"
+/usr/sbin/sysrc -f "${mnt}/etc/rc.conf" linux_enable="YES"
+
+######################################################################
+# Set ifconfig SYNCDHCP
+######################################################################
+
+chroot $mnt sh <<EOF
+#!/bin/sh
+ifconfig -l | tr ' ' '\n' | while read line ; do if [ "\$line" != "lo0" -a "\$line" != "pflog0" ]; then sysrc ifconfig_\${line}=DHCP ; fi ; done
+EOF
+
+######################################################################
+# Set SSH options
+######################################################################
+
+echo PermitRootLogin yes >> "${mnt}/etc/ssh/sshd_config"
+echo UseDNS no >> "${mnt}/etc/ssh/sshd_config"
+
+######################################################################
+# Change password of new system to blank
+######################################################################
+
+yes '' | chroot $mnt passwd
+
+######################################################################
+# Actually make the MFSROOT
+######################################################################
+
+if [ "$MAKEMFSROOT" ]; then
+dd if=/dev/zero of=${mnt}/${bpool}/mfsroot bs=512 count=245760
+mdevice=`mdconfig -a -t vnode -f ${mnt}/${bpool}/mfsroot`
+install -d -m 755 /mnt2
+newfs /dev/${mdevice}
+mount /dev/${mdevice} /mnt2
+########## Copy everything except /usr
+tar -c -f - \
+--exclude $bpool \
+--exclude /boot \
+--exclude null \
+--exclude ${release} \
+--exclude usr \
+-C ${mnt} ./ | tar -C /mnt2 -x -f -
+########## rc script for tmpfs /usr
+########## modified from https://github.com/mmatuska/mfsbsd
+cat >/mnt2/etc/rc.d/mdinit <<EOF
+#!/bin/sh
+# \$Id\$
+# PROVIDE: mdinit
+# BEFORE: zfs FILESYSTEMS
+# REQUIRE: mountcritlocal
+# KEYWORD: FreeBSD
+. /etc/rc.subr
+name="mdinit"
+start_cmd="mdinit_start"
+stop_cmd=":"
+mdinit_start()
+{
+	if [ -f /.usr.tar.xz ]; then
+    /rescue/test -d /usr || /rescue/mkdir /usr
+		/rescue/test -d /usr && /rescue/mount -t tmpfs tmpfs /usr
+		/rescue/test -d /usr && /rescue/tar -x -C / -f /.usr.tar.xz
+	elif [ -f /.usr.tar.bz2 ]; then
+    /rescue/test -d /usr || /rescue/mkdir /usr
+		/rescue/test -d /usr && /rescue/mount -t tmpfs tmpfs /usr
+		/rescue/test -d /usr && /rescue/tar -x -C / -f /.usr.tar.bz2
+	elif [ -f /.usr.tar.gz ]; then
+    /rescue/test -d /usr || /rescue/mkdir /usr
+		/rescue/test -d /usr && /rescue/mount -t tmpfs tmpfs /usr
+		/rescue/test -d /usr && /rescue/tar -x -C / -f /.usr.tar.gz
+	fi
+  if [ ! -f /usr/bin/which ]; then
+    echo "Something went wrong in mdinit. Entering shell:"
+    /rescue/sh
+  fi
+}
+load_rc_config \$name
+run_rc_command "\$1"
+EOF
+chmod 555 /mnt2/etc/rc.d/mdinit
+########## Package /usr
+tar -c -J -f /mnt2/.usr.tar.xz --exclude ${release} --options xz:compression-level=9 -C ${mnt} usr
+########## Unmount
+umount /dev/${mdevice}
+mdconfig -d -u ${mdevice#md}
+########## mfs_ settings in loader.conf
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" mfs_load="YES"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" mfs_type="mfs_root"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" mfs_name="/mfsroot"
+/usr/sbin/sysrc -f "${mnt}/boot/loader.conf" "vfs.root.mountfrom=ufs:/dev/md0"
+fi
+
+######################################################################
+# Remind user that they should change password of new system
+######################################################################
+
+cat <<EOF
+You should probably change the root password of the new system:
+       chroot $mnt passwd
+Then export both pools:
+       zpool export $bpool
+       zpool export $rpool
+EOF
